4b33ceaa7bcfc5c1ce44fa8229e488cfc038b2a2
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2023-12-09 17:51:29 +0300

    interface added

---
+++ rpgAPI/Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,11 @@
+﻿namespace rpgAPI.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+
+        Character GetCharacterById(int id);
+        List<Character> AddCharacter(Character newCharacter);
+     
+    }
+}
