270177f2a08d7ee9edbaaf96d121668ddd2b25dc
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2023-12-09 23:40:15 +0300

    DELETE added

--- rpgAPI/Services/CharacterService/CharacterService.cs
+++ rpgAPI/Services/CharacterService/CharacterService.cs
@@ -31,6 +31,33 @@ namespace rpgAPI.Services.CharacterService
             return serviceResponse;
         }
 
+        public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            try
+            {
+                var character = characters.FirstOrDefault(c => c.Id == id);
+
+                if (character is null)
+
+                    throw new Exception($"Character with Id '{id} not found.");
+
+
+
+                characters.Remove(character);
+
+                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+
+
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+            return serviceResponse;
+        }
+
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
@@ -51,7 +78,18 @@ namespace rpgAPI.Services.CharacterService
         public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
+            try
+            {
+
+            
+         
             var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+
+                if(character is null)
+                
+                    throw new Exception($"Character with Id '{updatedCharacter.Id} not found.");
+
+             
 
             character.Name = updatedCharacter.Name;
             character.HitPoints = updatedCharacter.HitPoints;
@@ -62,9 +100,13 @@ namespace rpgAPI.Services.CharacterService
 
             serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
 
+           
+            }catch(Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
             return serviceResponse;
-
-
         }
     }
 }
--- rpgAPI/Services/CharacterService/ICharacterService.cs
+++ rpgAPI/Services/CharacterService/ICharacterService.cs
@@ -9,5 +9,7 @@ namespace rpgAPI.Services.CharacterService
         Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
         Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
         Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
+        Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id);
+
     }
 }
